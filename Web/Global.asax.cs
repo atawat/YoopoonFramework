4b2a384a2470f4f0cb30a580a8369a4a2bb70b60
v-yuedua <v-yuedua@SADTT-YUEDUA-1>
2016-10-18 16:12:53 +0800

    添加演示web项目框架

---
+++ Web/Global.asax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using YooPoon.Core.Autofac;
+
+namespace Web
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            #region IOC配置
+
+            //DI配置
+            var initialize = new InitializeContainer();
+            initialize.Initializing();
+            //WebAPI
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new AutofacWebApiDependencyResolver(initialize.ContainerManager.Container);
+
+            //MVC
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(initialize.ContainerManager.Container));
+
+            #endregion
+        }
+    }
+}
